acb34451e2947c109b5008b14b6690e956a487dc
Dong Wang <dwangrz@hotmail.com>
2023-05-26 23:02:55 +0200

    Remove unused import

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -1,7 +1,6 @@
 import time
 import pandas as pd
 import numpy as np
-from IPython.display import display
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
